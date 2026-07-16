f5735995979d55b60e08795f7074698bc4c0801b
Smallcoder <VISISTU01@GMAIL.COM>
2026-07-16 21:39:19 +0800

    rebuild: expose only subconscious core API

--- src/maces/__init__.py
+++ src/maces/__init__.py
@@ -1,38 +1,25 @@
-from .adapters import GenericMemoryAdapter, HermesRuntimeAdapter
-from .capabilities import ApprovalProvider, CanonicalProvider, CapabilityBus, ResearchProvider
 from .engine import MacesEngine
 from .influence import InfluenceEngine
-from .learning import LearningExecutor, LearningStrategy
 from .models import (
     CognitiveEvent,
+    EventKind,
     InfluenceSignal,
-    LearningIntent,
     LearningProposal,
     PromotionProposal,
-    ResearchPlan,
     StagedArtifact,
 )
 from .policy import MacesPolicy
 from .store import CognitiveStore
 
 __all__ = [
-    "ApprovalProvider",
-    "CanonicalProvider",
-    "CapabilityBus",
     "CognitiveEvent",
     "CognitiveStore",
-    "GenericMemoryAdapter",
-    "HermesRuntimeAdapter",
+    "EventKind",
     "InfluenceEngine",
     "InfluenceSignal",
-    "LearningExecutor",
-    "LearningIntent",
     "LearningProposal",
-    "LearningStrategy",
     "MacesEngine",
     "MacesPolicy",
     "PromotionProposal",
-    "ResearchPlan",
-    "ResearchProvider",
     "StagedArtifact",
 ]
